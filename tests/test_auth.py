53c94a05ab3ec5bae397c5e598dc27f1a0c9fba7
Jan Peterka <janmpeterka@gmail.com>
2019-09-12 14:56:45 +0200

    Add testing create_user helper

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,7 +5,6 @@ from app.models import User
 def test_login(app, client):
     assert client.get("/login") == 200
     # username
-    # assert do_login("admin", "adminadmin") is True
     assert do_login("admin", "admin") is False
     assert do_login("", "") is False
     # TODO: OAuth @TEST (30)
@@ -17,23 +16,12 @@ def test_logout(app, client):
 
 
 def test_register(db):
-    user = User(username="test")
-    user.password = "testtest"
-    user.set_password_hash(user.password.encode("utf-8"))
-    user.first_name = "TEST"
-    user.last_name = "TEST"
-    user.password_version = "bcrypt"
+    user = helper_create_user("test", "testtest")
 
     assert do_register(user) is True
     assert do_login(user.username, user.password) is True
 
-    # duplicate username
-    another_user = User(username="test")
-    another_user.password = "otherpassword"
-    another_user.set_password_hash(another_user.password.encode("utf-8"))
-    another_user.first_name = "TEST"
-    another_user.last_name = "TEST"
-    another_user.password_version = "bcrypt"
+    another_user = helper_create_user("test", "otherpassword")
 
     assert do_register(another_user) is False
 
@@ -51,3 +39,14 @@ def test_new_password(app, client, db):
 
     # TODO: try already used token @TEST (20)
     pass
+
+
+def helper_create_user(username, password):
+    user = User(username)
+    user.password = password
+    user.set_password_hash(user.password.encode("utf-8"))
+    user.first_name = "TEST"
+    user.last_name = "TEST"
+    user.password_version = "bcrypt"
+
+    return user
