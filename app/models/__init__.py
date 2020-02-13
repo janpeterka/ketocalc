cc44fdc35df4db23ea9ef455b97d8fc8a90e98b1
Jan Peterka <janmpeterka@gmail.com>
2020-02-13 14:22:53 +0100

    Fix wrong naming

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@
 # import all tables that are not classes (only raw M:N relationship tables)
 # imports automatically (because it's __init__.py file)
 
-from .models.diets_has_recipes import diets_has_recipes  # noqa: F401
-from .models.users_has_diets import users_has_diets  # noqa: F401
+from .diets_has_recipes import diets_has_recipes  # noqa: F401
+from .users_has_diets import users_has_diets  # noqa: F401
