9d51fb4c785bf671d8dbaee13b7720b4b8963786
Jan Peterka <janmpeterka@gmail.com>
2020-02-17 20:22:15 +0100

    Add templates, disable trial

--- app/controllers/trial_recipes.py
+++ app/controllers/trial_recipes.py
@@ -1,5 +1,7 @@
 from flask import render_template as template
+from flask import redirect, url_for
 
+from flask_login import current_user
 from flask_classful import FlaskView
 
 from app.models.ingredients import Ingredient
@@ -7,6 +9,10 @@ from app.models.users import User
 
 
 class TrialRecipesView(FlaskView):
+    def before_show(self):
+        if current_user.is_authenticated:
+            return redirect(url_for("IndexView:index"))
+
     def show(self):
         active_diets = User.load(
             "ketocalc.jmp@gmail.com", load_type="username"
