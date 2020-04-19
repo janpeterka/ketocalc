367d48e4b53b176d42e953fe860c71da67c490c0
janmpeterka <janmpeterka@gmail.com>
2020-04-19 16:20:45 +0200

    Add request logging (#109)

--- ketocalc.py
+++ ketocalc.py
@@ -1,11 +1,15 @@
 import os
+import re
 from flask import request, redirect
+
+from flask_login import current_user
 
 from app import create_app
 
 # from app.models import db
 # from app import db
 # from app.models.users import User
+from app.models.request_log import RequestLog
 
 from app.data import template_data
 
@@ -27,6 +31,20 @@ def session_management():
         return redirect("/shutdown")
     elif request.path == "/shutdown" and application.config["APP_STATE"] != "shutdown":
         return redirect("/")
+
+
+@application.before_request
+def log_request():
+    pattern = re.compile("^/static/([A-Za-z0-9])*$")
+    if not pattern.match(request.path):
+        url = request.path
+        remote_addr = request.environ["REMOTE_ADDR"]
+        if hasattr(current_user, "id"):
+            user_id = current_user.id
+        else:
+            user_id = None
+        log = RequestLog(url=url, user_id=user_id, remote_addr=remote_addr)
+        log.save()
 
 
 # @application.shell_context_processor
