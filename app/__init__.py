b4411ba67310967c699d0c5d1d51cd913ff99c70
janmpeterka <janmpeterka@gmail.com>
2020-04-21 22:52:11 +0200

    Disable autoflush (#117)
    * Disable autoflush
    
    * Revert "Disable autoflush"
    
    This reverts commit 9b40e31348202577bcc8963c621ae62e78cf6dd5.
    
    * Disable autoflush

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ pymysql.converters.conversions = pymysql.converters.encoders.copy()
 pymysql.converters.conversions.update(pymysql.converters.decoders)
 
 mail = Mail()
-db = SQLAlchemy()
+db = SQLAlchemy(session_options={"autoflush": False})
 migrate = Migrate()
 
 
