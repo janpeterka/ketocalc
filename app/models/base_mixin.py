136086da84894f68387b74a119584c02cbc1d4f3
janmpeterka <janmpeterka@gmail.com>
2020-04-21 23:59:08 +0200

    Try to fix saving (#118)
    * Try to fix wrong saving of changed ingredients
    
    * Fix broken logs

--- app/models/base_mixin.py
+++ app/models/base_mixin.py
@@ -34,6 +34,7 @@ class BaseMixin(object):
         """Saves (new) object
         """
         try:
+            db.session.expire_all()
             db.session.add(self)
             db.session.commit()
             if self.id is not None:
