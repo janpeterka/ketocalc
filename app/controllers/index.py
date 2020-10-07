8868cfa4be6dad0772cc823313c63e9e7d169641
janmpeterka <jan.peterka@hey.com>
2020-10-07 15:30:41 +0200

    Add app function explanation (#172)
    * Update styles, add fonts
    
    * Add new index page
    
    * Add missing link
    
    Co-authored-by: Jan Peterka <janmpeterka@gmail.com>

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -1,4 +1,5 @@
 from flask import redirect, url_for
+from flask import render_template as template
 
 from flask_classful import FlaskView
 from flask_login import current_user
@@ -11,4 +12,5 @@ class IndexView(FlaskView):
         if current_user.is_authenticated:
             return redirect(url_for("DashboardView:index"))
         else:
-            return redirect(url_for("LoginView:show"))
+            # return redirect(url_for("LoginView:show"))
+            return template("index/index.html.j2")
