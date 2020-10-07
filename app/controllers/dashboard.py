8b0fb0d9ba9627b6e0b414868dd7785a1a91e961
Jan Peterka <janmpeterka@gmail.com>
2020-10-08 01:33:03 +0200

    Add daily_plans widget to dashboard

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -1,13 +1,17 @@
-from flask import render_template as template
+import datetime
+
 from flask import request, redirect, url_for
 
-from flask_classful import FlaskView
+# from flask_classful import FlaskView
 from flask_login import current_user, login_required
 
 from app.models.diets import Diet
+from app.models.daily_plans import DailyPlan
+
+from app.controllers.extended_flask_view import ExtendedFlaskView
 
 
-class DashboardView(FlaskView):
+class DashboardView(ExtendedFlaskView):
     decorators = [login_required]
 
     def before_index(self):
@@ -17,15 +21,17 @@ class DashboardView(FlaskView):
 
     def index(self):
         if self.selected_diet_id is None and len(current_user.active_diets) > 0:
-            selected_diet = current_user.active_diets[0]
+            self.selected_diet = current_user.active_diets[0]
         else:
-            selected_diet = Diet.load(self.selected_diet_id)
+            self.selected_diet = Diet.load(self.selected_diet_id)
 
-        return template(
-            "dashboard/dashboard.html.j2",
-            diets=current_user.active_diets,
-            selected_diet=selected_diet,
-            first_name=current_user.first_name,
+        self.diets = current_user.active_diets
+
+        self.daily_plan = DailyPlan.load_by_date(date=datetime.date.today())
+        self.daily_recipes = self.daily_plan.has_recipes
+
+        return self.template(
+            "dashboard/dashboard.html.j2", first_name=current_user.first_name,
         )
 
     def show(self):
