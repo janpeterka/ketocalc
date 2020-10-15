fd2b717570f60929205e9af6b7251536f403a1e0
Jan Peterka <janmpeterka@gmail.com>
2020-10-15 18:34:09 +0200

    Add files model

---
+++ app/controllers/files.py
@@ -0,0 +1,36 @@
+from flask import abort
+
+from flask_security import current_user
+
+from flask_classful import FlaskView
+
+from app.models.files import File
+
+# from app.models.users import User
+from app.handlers.files import FileHandler
+
+
+class FilesView(FlaskView):
+    def show(self, hash_value):
+        file = File.load_by_attribute("hash", hash_value)
+        if not file:
+            abort(404)
+        if not file.can_view(current_user):
+            abort(403)
+        return FileHandler().show(file)
+
+    # def show_profile_pic(self, user_id):
+    #     file = User.load(user_id).profile_picture_file
+    #     if not file:
+    #         return FileHandler().show_new_user_placeholder()
+    #     if not file.can_view(current_user):
+    #         abort(403)
+    #     return FileHandler().show(file)
+
+    def download(self, id):
+        file = File.load(id)
+        if not file:
+            abort(404)
+        if not file.can_view(current_user):
+            abort(403)
+        return FileHandler().download(file)
