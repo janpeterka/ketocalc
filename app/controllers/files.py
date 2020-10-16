979c4c1ec3387d5ca3d7cbe12c1fb420876de679
Jan Peterka <janmpeterka@gmail.com>
2020-10-16 18:14:34 +0200

    Add admin file view

--- app/controllers/files.py
+++ app/controllers/files.py
@@ -1,13 +1,16 @@
 from flask import abort
+from flask import render_template as template
 
 from flask_security import current_user
 
 from flask_classful import FlaskView
 
 from app.models.files import File
+from app.models.files import RecipeImageFile
 
 # from app.models.users import User
 from app.handlers.files import FileHandler
+from app.handlers.files import AWSFileHandler
 
 
 class FilesView(FlaskView):
@@ -34,3 +37,13 @@ class FilesView(FlaskView):
         if not file.can_view(current_user):
             abort(403)
         return FileHandler().download(file)
+
+    def index(self):
+        aws_files = AWSFileHandler().list_files()
+        files = []
+        for aws_file in aws_files:
+            # file = File().load_by_attribute("path", aws_file['Key'])
+            file = RecipeImageFile().load_by_attribute("path", aws_file["Key"])
+            if file is not None:
+                files.append(file)
+        return template("admin/files/all.html.j2", files=files)
