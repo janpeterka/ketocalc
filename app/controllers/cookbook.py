a564a04ae0bc7b2eb272e879cd974d8b78077f94
janmpeterka <jan.peterka@hey.com>
2020-10-17 13:56:08 +0200

    Add cookbook filter table (#188)
    * Add simple filtering
    
    * Add sort to admin user table
    
    Co-authored-by: Jan Peterka <janmpeterka@gmail.com>

--- app/controllers/cookbook.py
+++ app/controllers/cookbook.py
@@ -1,10 +1,13 @@
 from app.models.recipes import Recipe
 
+from flask_login import login_required
+
 from app.controllers.extended_flask_view import ExtendedFlaskView
 
 
 class CookbookView(ExtendedFlaskView):
     template_folder = "cookbook"
+    decorators = [login_required]
 
     def before_index(self):
         self.recipes = Recipe.public_recipes()
