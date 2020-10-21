00881fd1d286831494fde619762c7175eeed1341
Jan Peterka <janmpeterka@gmail.com>
2020-10-21 23:19:55 +0200

    Add filter

--- app/controllers/cookbook.py
+++ app/controllers/cookbook.py
@@ -1,11 +1,14 @@
 from app.models.recipes import Recipe
 
-from flask import redirect, url_for
+from flask import redirect, url_for, request
+
+from flask_classful import route
 
 from flask_login import current_user
 
 from app.data.texts import texts
-from app.controllers.extended_flask_view import ExtendedFlaskView
+from .extended_flask_view import ExtendedFlaskView
+from .forms.cookbook_filter import CookbookFilterForm
 
 
 class CookbookView(ExtendedFlaskView):
@@ -17,3 +20,44 @@ class CookbookView(ExtendedFlaskView):
             return redirect(url_for("CookbookView:not_logged_in", message=message))
 
         self.recipes = Recipe.public_recipes()
+        # Get values for filters
+        ingredients = [x.ingredients for x in self.recipes]
+        flatten_ingredients = [y for x in ingredients for y in x]
+        ingredient_names = [x.name for x in flatten_ingredients]
+        self.ingredient_names = ["--všechny--"]
+        self.ingredient_names.extend(list(set(ingredient_names)))
+        self.ingredient_names.sort()
+
+        if request.method == "GET":
+            self.form = CookbookFilterForm(ingredient_names=self.ingredient_names)
+        else:
+            self.form = CookbookFilterForm(
+                request.form, ingredient_names=self.ingredient_names
+            )
+
+        # Get filters from request
+        ingredient_name = None
+        ratio_from = None
+        ratio_to = None
+
+        if request.method == "POST":
+            if not self.form.ingredient_name.data == "--všechny--":
+                ingredient_name = self.form.ingredient_name.data
+            ratio_from = self.form.ratio_from.data
+            ratio_to = self.form.ratio_to.data
+
+        # Filter recipes
+        if ingredient_name:
+            self.recipes = [
+                x for x in self.recipes if ingredient_name in x.concat_ingredients
+            ]
+
+        if ratio_from:
+            self.recipes = [x for x in self.recipes if x.ratio >= ratio_from]
+
+        if ratio_to:
+            self.recipes = [x for x in self.recipes if x.ratio <= ratio_to]
+
+    @route("/", methods=["GET", "POST"])
+    def index(self):
+        return self.template()
