8c202ee29af294b61bf358ee02138c87051d7980
Jan Peterka <janmpeterka@gmail.com>
2020-10-22 15:30:36 +0200

    Update template code

--- app/controllers/errors.py
+++ app/controllers/errors.py
@@ -1,5 +1,4 @@
 from flask import abort, make_response
-from flask import current_app as application
 from flask import render_template as template
 
 from flask_classful import FlaskView
@@ -8,10 +7,6 @@ from app.auth import admin_required
 
 
 class ErrorsView(FlaskView):
-    @admin_required
-    def raise_error(self):
-        abort(500)
-
     def wrongpage(self):
         abort(405)
 
@@ -34,17 +29,14 @@ class ErrorsView(FlaskView):
 
 def error404(error):
     # Missing page
-    # application.logger.info(str(error))
     return template("errors/err404.html.j2"), 404
 
 
 def error405(error=None):
     # Action not allowed
-    # application.logger.info(str(error))
     return make_response(template("errors/err405.html.j2"), 405)
 
 
 def error500(error):
     # Internal error
-    application.logger.error(str(error))
     return make_response(template("errors/err500.html.j2"), 500)
