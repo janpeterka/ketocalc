8f00bcd4eb3cf4b633bdf5e763808dffa627f55b
Jan Peterka <janmpeterka@gmail.com>
2020-11-28 16:02:16 +0100

    Reformat test data to dictionaries

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,74 +41,127 @@ def db_fill_calc():
     user = create_user(username="calc", password="calc_clack")
     user.save()
 
-    Diet(
-        name="3.5", calorie=0, sugar=10, fat=81, protein=13, active=1, user_id=1,
-    ).save()
+    diets = [
+        {
+            "name": "3.5",
+            "calorie": 0,
+            "sugar": 10,
+            "fat": 81,
+            "protein": 13,
+            "active": 1,
+            "user_id": 1,
+        }
+    ]
 
-    Ingredient(
-        name="Brambory skladované",
-        calorie=219,
-        sugar=12.2,
-        fat=0.1,
-        protein=1.1,
-        author="calc",
-    ).save()
-    Ingredient(
-        name="Česnek", calorie=366, sugar=21.8, fat=0.2, protein=5.4, author="calc"
-    ).save()
-    Ingredient(
-        name="Cuketa", calorie=57, sugar=1.8, fat=0.1, protein=0.8, author="calc"
-    ).save()
-    Ingredient(
-        name="Filé z Aljašky",
-        calorie=352,
-        sugar=0,
-        fat=2.76,
-        protein=14.7,
-        author="calc",
-    ).save()
-    Ingredient(
-        name="Kurkuma", calorie=1435, sugar=65.9, fat=2.5, protein=10.8, author="calc"
-    ).save()
-    Ingredient(
-        name="Máslo výběrové",
-        calorie=3056,
-        sugar=0.6,
-        fat=82,
-        protein=0.7,
-        author="calc",
-    ).save()
-    Ingredient(
-        name="Okurka salátová", calorie=54, sugar=2.1, fat=0.2, protein=1, author="calc"
-    ).save()
-    # for do_register add_default_ingredients confirmation
-    Ingredient(
-        name="Default salátová",
-        calorie=100,
-        sugar=2.1,
-        fat=0.2,
-        protein=1,
-        author="default",
-        is_shared=True,
-        is_approved=True,
-    ).save()
-    Ingredient(
-        name="Okurka defaultová",
-        calorie=54,
-        sugar=100,
-        fat=0.2,
-        protein=1,
-        author="default",
-        is_shared=True,
-        is_approved=True,
-    ).save()
-    Ingredient(
-        name="Okurka salátová",
-        calorie=54,
-        sugar=2.1,
-        fat=100,
-        protein=1,
-        author="default",
-        is_shared=True,
-        is_approved=True,
-    ).save()
+    for diet in diets:
+        Diet(
+            name=diet["name"],
+            calorie=diet["calorie"],
+            sugar=diet["sugar"],
+            fat=diet["fat"],
+            protein=diet["protein"],
+            active=diet["active"],
+            user_id=diet["user_id"],
+        ).save()
+
+    ingredients = [
+        {
+            "name": "Brambory skladované",
+            "calorie": 219,
+            "sugar": 12.2,
+            "fat": 0.1,
+            "protein": 1.1,
+            "author": "calc",
+        },
+        {
+            "name": "Česnek",
+            "calorie": 366,
+            "sugar": 21.8,
+            "fat": 0.2,
+            "protein": 5.4,
+            "author": "calc",
+        },
+        {
+            "name": "Cuketa",
+            "calorie": 57,
+            "sugar": 1.8,
+            "fat": 0.1,
+            "protein": 0.8,
+            "author": "calc",
+        },
+        {
+            "name": "Filé z Aljašky",
+            "calorie": 352,
+            "sugar": 0,
+            "fat": 2.76,
+            "protein": 14.7,
+            "author": "calc",
+        },
+        {
+            "name": "Kurkuma",
+            "calorie": 1435,
+            "sugar": 65.9,
+            "fat": 2.5,
+            "protein": 10.8,
+            "author": "calc",
+        },
+        {
+            "name": "Máslo výběrové",
+            "calorie": 3056,
+            "sugar": 0.6,
+            "fat": 82,
+            "protein": 0.7,
+            "author": "calc",
+        },
+        {
+            "name": "Okurka salátová",
+            "calorie": 54,
+            "sugar": 2.1,
+            "fat": 0.2,
+            "protein": 1,
+            "author": "calc",
+        },
+        # for do_register add_default_ingredients confirmation
+        {
+            "name": "Default salátová",
+            "calorie": 100,
+            "sugar": 2.1,
+            "fat": 0.2,
+            "protein": 1,
+            "author": "default",
+            "is_shared": True,
+            "is_approved": True,
+        },
+        {
+            "name": "Okurka defaultová",
+            "calorie": 54,
+            "sugar": 100,
+            "fat": 0.2,
+            "protein": 1,
+            "author": "default",
+            "is_shared": True,
+            "is_approved": True,
+        },
+        {
+            "name": "Okurka salátová",
+            "calorie": 54,
+            "sugar": 2.1,
+            "fat": 100,
+            "protein": 1,
+            "author": "default",
+            "is_shared": True,
+            "is_approved": True,
+        },
+    ]
+
+    for ingredient in ingredients:
+        Ingredient(
+            name=ingredient["name"],
+            calorie=ingredient["calorie"],
+            sugar=ingredient["sugar"],
+            fat=ingredient["fat"],
+            protein=ingredient["protein"],
+            author=ingredient["author"],
+            is_shared=getattr(ingredient, "is_shared", None),
+            is_approved=getattr(ingredient, "is_approved", None),
+        ).save()
