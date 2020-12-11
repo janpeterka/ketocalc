2ce662f1a6b68c23c8881a93ef978858b3cd5f53
Jan Peterka <janmpeterka@gmail.com>
2020-12-11 14:44:31 +0100

    Add (possibly random) preset ingredients to trial recipe

--- app/controllers/trial_recipes.py
+++ app/controllers/trial_recipes.py
@@ -15,9 +15,30 @@ class TrialRecipesView(BaseRecipesView):
 
     def index(self):
         shared_user = User.load_shared_user()
+        preset_ingredients = []
+
+        ingredient = Ingredient.load_shared_by_name("Ananas")
+        if ingredient:
+            preset_ingredients.append(ingredient)
+        else:
+            preset_ingredients.append(Ingredient.load_random_by_nutrient("sugar"))
+
+        ingredient = Ingredient.load_shared_by_name("Avokádo")
+        if ingredient:
+            preset_ingredients.append(ingredient)
+        else:
+            preset_ingredients.append(Ingredient.load_random_by_nutrient("fat"))
+
+        ingredient = Ingredient.load_shared_by_name("Maso - krůtí, prsa bez kosti")
+        if ingredient:
+            preset_ingredients.append(ingredient)
+        else:
+            preset_ingredients.append(Ingredient.load_random_by_nutrient("protein"))
+
         return template(
             "recipes/new.html.j2",
             ingredients=Ingredient.load_all_shared(),
+            preset_ingredients=[i.id for i in preset_ingredients],
             diets=shared_user.active_diets,
             is_trialrecipe=True,
         )
