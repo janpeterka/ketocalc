3cf470caf1efa7ab178584480840a6328c34c397
Jan Peterka <janmpeterka@gmail.com>
2020-12-25 23:19:39 +0100

    Fix wrong admin dashboard recipe like toggle value

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -19,15 +19,16 @@ class AdminView(ExtendedFlaskView):
     decorators = [admin_required]
 
     def index(self):
+        from app.helpers.general import created_recently
+
         self.days = 30
         self.new_users = User.created_recently(days=self.days)
         self.new_recipes = Recipe.created_recently(days=self.days)
         self.new_ingredients = Ingredient.created_recently(days=self.days)
         self.new_images = ImageFile.created_recently(days=self.days)
-        self.share_recipe_toggles = RequestLog.load_by_like(
-            attribute="url", pattern="recipes/toggle_shared"
+        self.share_recipe_toggles = created_recently(
+            RequestLog.load_by_like(attribute="url", pattern="recipes/toggle_shared"),
+            days=self.days,
         )
-
-        self.test = User.load_all()
 
         return self.template()
