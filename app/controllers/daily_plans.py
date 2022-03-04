b72496b589e30d3aa1310ce3e3a6010314707fc4
Jan Peterka <jan.peterka@hey.com>
2022-03-04 13:36:08 +0100

    Hotfix login not required

--- app/controllers/daily_plans.py
+++ app/controllers/daily_plans.py
@@ -15,11 +15,10 @@ from app.controllers.extended_flask_view import ExtendedFlaskView
 
 
 class DailyPlansView(ExtendedFlaskView):
-    def before_index(self):
-        if not current_user.is_authenticated:
-            message = texts.daily_plan.not_logged_in
-            return redirect(url_for("DailyPlansView:not_logged_in", message=message))
+    decorators = [login_required]
 
+
+    @login_required
     def before_request(self, name, id=None, *args, **kwargs):
         super().before_request(name, id, *args, **kwargs)
 
@@ -29,11 +28,14 @@ class DailyPlansView(ExtendedFlaskView):
                 self.date = parse_date(self.date)
             self.daily_plan = DailyPlan.load_by_date_or_create(self.date)
 
-    @login_required
+    def before_index(self):
+        if not current_user.is_authenticated:
+            message = texts.daily_plan.not_logged_in
+            return redirect(url_for("DailyPlansView:not_logged_in", message=message))
+    
     def index(self):
         return redirect(url_for("DailyPlansView:show", date=datetime.date.today()))
 
-    @login_required
     def show(self, date):
         date_before = self.date + datetime.timedelta(days=-1)
         date_after = self.date + datetime.timedelta(days=1)
@@ -46,14 +48,12 @@ class DailyPlansView(ExtendedFlaskView):
 
         return self.template()
 
-    @login_required
     def remove_daily_recipe(self, daily_recipe_id, date):
         daily_plan = DailyPlan.load_by_date_or_create(date)
         daily_plan.remove_daily_recipe_by_id(daily_recipe_id)
         return redirect(url_for("DailyPlansView:show", date=date))
 
     @route("/add_recipe", methods=["POST"])
-    @login_required
     def add_recipe(self):
         recipe = Recipe.load(request.form["recipe_id"])
         if not recipe.can_current_user_add:
